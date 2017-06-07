1062753169964149752038eaaf6819b60ca959d7
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2017-06-07 11:25:25 -0700

    Make it work on Windows

--- myElement.hpp
+++ myElement.hpp
@@ -72,7 +72,7 @@ namespace ngfem
 
 #ifdef NGS_PYTHON
 #include <python_ngstd.hpp>
-void ExportMyElement(py::module m);
+MYNGS_DLL_HEADER void ExportMyElement(py::module m);
 #endif // NGS_PYTHON
 
 #endif // FILE_MYELEMENT_HPP
