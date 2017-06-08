9c77d165d99fde4a4d714a2bd6aad95a80b051ba
Christopher Lackner <christopher.lackner@tuwien.ac.at>
2017-06-08 13:14:47 +0200

    fix 1-3 allinone

--- 1_Basic/3_only_c++/all_in_one.cpp
+++ 1_Basic/3_only_c++/all_in_one.cpp
@@ -7,8 +7,9 @@
 
 /*
 
-We write one numproc which creates and maintains all objects to solve
-the Poisson equation.
+In this example we show how to use NGSolve C++ code only, without any interface.
+We will solve a poisson problem here.
+For this the mesh must be created by Netgen and stored in the file square.vol
 
 */
 
@@ -16,81 +17,58 @@ the Poisson equation.
 
 using namespace ngsolve;
 
-namespace all_in_one
+int main(int argc, char** argv)
 {
-  class NumProcAllInOne : public NumProc
-  {
-  protected:
-    shared_ptr<FESpace> fes;
-    shared_ptr<GridFunction> gfu;
-    shared_ptr<BilinearForm> bfa;
-    shared_ptr<LinearForm> lff;
+  cout << "Start AllInOne" << endl;
 
-  public:
-    
-    NumProcAllInOne (shared_ptr<PDE> apde, const Flags & flags)
-      : NumProc (apde)
-    { 
-      cout << "Constructor of NumProcAllInOne" << endl;
+  auto ma = make_shared<MeshAccess>("square.vol");
+  Flags flags_fes;
+  flags_fes.SetFlag ("order", 4);
+  auto fes = make_shared<H1HighOrderFESpace> (ma, flags_fes);
 
-      Flags flags_fes;
-      flags_fes.SetFlag ("order", 4);
-      fes = make_shared<H1HighOrderFESpace> (ma, flags_fes);
+  Flags flags_gfu;
+  auto gfu = make_shared<T_GridFunction<double>> (fes, "u", flags_gfu);
 
-      Flags flags_gfu;
-      gfu = make_shared<T_GridFunction<double>> (fes, "u", flags_gfu);
+  Flags flags_bfa;
+  auto bfa = make_shared<T_BilinearFormSymmetric<double>> (fes, "a", flags_bfa);
 
-      Flags flags_bfa;
-      bfa = make_shared<T_BilinearFormSymmetric<double>> (fes, "a", flags_bfa);
+  shared_ptr<BilinearFormIntegrator> bfi =
+    make_shared<LaplaceIntegrator<2>> (make_shared<ConstantCoefficientFunction> (1));
+  bfa -> AddIntegrator (bfi);
 
-      shared_ptr<BilinearFormIntegrator> bfi = 
-        make_shared<LaplaceIntegrator<2>> (make_shared<ConstantCoefficientFunction> (1));
-      bfa -> AddIntegrator (bfi);
+  Array<double> penalty(ma->GetNBoundaries());
+  penalty = 0.0;
+  penalty[0] = 1e10;
 
-      Array<double> penalty(ma->GetNBoundaries());
-      penalty = 0.0;
-      penalty[0] = 1e10;
-
-      bfi = make_shared<RobinIntegrator<2>> (make_shared<DomainConstantCoefficientFunction> (penalty));
-      bfa -> AddIntegrator (bfi);
+  bfi = make_shared<RobinIntegrator<2>> (make_shared<DomainConstantCoefficientFunction> (penalty));
+  bfa -> AddIntegrator (bfi);
 
 
-      bfi = make_shared<MassIntegrator<2>> (make_shared<ConstantCoefficientFunction> (1));
-      bfa -> AddIntegrator (bfi);
+  bfi = make_shared<MassIntegrator<2>> (make_shared<ConstantCoefficientFunction> (1));
+  bfa -> AddIntegrator (bfi);
 
-      Flags flags_lff;
-      lff = make_shared<T_LinearForm<double>> (fes, "f", flags_lff);
+  Flags flags_lff;
+  auto lff = make_shared<T_LinearForm<double>> (fes, "f", flags_lff);
 
-      auto lfi = make_shared<SourceIntegrator<2>> (make_shared<ConstantCoefficientFunction> (5));
-      lff -> AddIntegrator (lfi);
-    }
+  auto lfi = make_shared<SourceIntegrator<2>> (make_shared<ConstantCoefficientFunction> (5));
+  lff -> AddIntegrator (lfi);
 
-    virtual string GetClassName () const
-    {
-      return "AllInOne";
-    }
+  LocalHeap lh(100000);
+  fes -> Update(lh);
+  fes -> FinalizeUpdate(lh);
 
-    virtual void Do (LocalHeap & lh)
-    {
-      fes -> Update(lh);
-      fes -> FinalizeUpdate(lh);
+  gfu -> Update();
+  bfa -> Assemble(lh);
+  lff -> Assemble(lh);
 
-      gfu -> Update();
-      bfa -> Assemble(lh);
-      lff -> Assemble(lh);
+  const BaseMatrix & mata = bfa -> GetMatrix();
+  const BaseVector & vecf = lff -> GetVector();
+  BaseVector & vecu = gfu -> GetVector();
 
-      const BaseMatrix & mata = bfa -> GetMatrix();
-      const BaseVector & vecf = lff -> GetVector();
-      BaseVector & vecu = gfu -> GetVector();
+  auto inverse = mata.InverseMatrix(fes->GetFreeDofs());
 
-      auto inverse = mata.InverseMatrix(fes->GetFreeDofs());
-      
-      vecu = *inverse * vecf;
-    }
-  };
+  vecu = *inverse * vecf;
 
-  
-
-
-  static RegisterNumProc<NumProcAllInOne> npinit1("allinone");
+  cout << "Solution vector = " << endl << vecu << endl;
+  return 0;
 }
