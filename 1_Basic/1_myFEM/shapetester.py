a43e3b8282f46f0880623cbbc1a83736e9f76935
Christopher Lackner <christopher.lackner@tuwien.ac.at>
2017-06-12 14:04:57 +0200

    add shapetester

---
+++ 1_Basic/1_myFEM/shapetester.py
@@ -0,0 +1,19 @@
+from netgen.geom2d import unit_square
+from ngsolve import *
+from myngspy import *
+
+mesh = Mesh(unit_square.GenerateMesh(maxh=0.2))
+
+fes = FESpace("myfespace", mesh)
+
+u = GridFunction(fes,"shapes")
+
+Draw(u)
+
+# we can use the additionally exported function here
+for i in range(fes.GetNVert()):
+    print("Draw basis function ", i)
+    u.vec[:] = 0
+    u.vec[i] = 1
+    Redraw()
+    input("press key to draw next shape function")
