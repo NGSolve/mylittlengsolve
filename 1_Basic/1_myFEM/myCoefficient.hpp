59b864446405f4f989bfbafb264c218870c03430
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2019-06-27 14:53:51 +0200

    more

--- 1_Basic/1_myFEM/myCoefficient.hpp
+++ 1_Basic/1_myFEM/myCoefficient.hpp
@@ -15,20 +15,31 @@ namespace ngcomp
     virtual double Evaluate(const BaseMappedIntegrationPoint& mip) const override
     {
       static Timer timer("MyCoefficientFunction - Evaluate(BaseMappedIntegrationPoint)");
-      RegionTimer reg_timer(timer);
+      RegionTracer r_timer(TaskManager::GetThreadId(), timer);
       // maybe do something with mip and trafo?
       auto pnt = mip.GetPoint();
       return pnt[0] * pnt[1];
     }
 
     // ******************* Performance improvements **********************
+
+    virtual void Evaluate (const BaseMappedIntegrationRule & mir, BareSliceMatrix<double> hvalues) const override
+    {
+      static Timer timer("MyCoefficientFunction - Evaluate(BaseMappedIntegrationRule)");
+      RegionTracer r_timer(TaskManager::GetThreadId(), timer);
+      auto values = hvalues.AddSize(mir.Size(), Dimension());
+      auto points = mir.GetPoints();
+      for (int i = 0; i < mir.Size(); i++)
+        values(i,0) = points(i,0) * points(i,1);
+    }
+
   };
 
   void ExportMyCoefficient(py::module m);
 
   inline double MyIntegrate( shared_ptr<CoefficientFunction> cf, shared_ptr<MeshAccess> ma, int order )
   {
-    LocalHeap lh(1000000, "localheap");
+    LocalHeap lh(100000000, "localheap");
 
     double hsum = 0.0;
     for (auto elnum : Range(ma->GetNE(VOL)))
@@ -36,14 +47,56 @@ namespace ngcomp
       auto element = ma->GetElement(ElementId(VOL, elnum));
       auto & trafo = ma->GetTrafo (element, lh);
       IntegrationRule ir(trafo.GetElementType(), order);
-      BaseMappedIntegrationRule & mir = trafo(ir, lh);
       Matrix<> values(ir.Size(), 1);
+
+      BaseMappedIntegrationRule & mir = trafo(ir, lh);
       cf -> Evaluate (mir, values);
+
       for (int i = 0; i < values.Height(); i++)
         hsum += mir[i].GetWeight() * values(i,0);
     }
     return hsum;
   }
+}
+
+  /*
+  // Version including Timers
+  inline double MyIntegrate( shared_ptr<CoefficientFunction> cf, shared_ptr<MeshAccess> ma, int order )
+  {
+    static Timer t_all("MyIntegrate");
+    static Timer t_evaluate("Evaluate");
+    static Timer t_sum("Sum");
+    static Timer t_after_init("After init");
+
+    RegionTimer r_all(t_all);
+
+    LocalHeap lh(100000000, "localheap");
+
+    double hsum = 0.0;
+    for (auto elnum : Range(ma->GetNE(VOL)))
+    {
+      RegionTimer r_int_el(t_all);
+      auto element = ma->GetElement(ElementId(VOL, elnum));
+      auto & trafo = ma->GetTrafo (element, lh);
+      IntegrationRule ir(trafo.GetElementType(), order);
+      Matrix<> values(ir.Size(), 1);
+
+      RegionTimer r_after_init( t_after_init);
+      BaseMappedIntegrationRule & mir = trafo(ir, lh);
+      {
+        RegionTimer r_evaluate( t_evaluate);
+        cf -> Evaluate (mir, values);
+      }
+
+      {
+        RegionTimer r_sum( t_sum);
+        for (int i = 0; i < values.Height(); i++)
+          hsum += mir[i].GetWeight() * values(i,0);
+      }
+    }
+    return hsum;
+  }
+  */
 }
 
 
--- 1_Basic/1_myFEM/timings.py
+++ 1_Basic/1_myFEM/timings.py
@@ -3,24 +3,27 @@ from ngsolve import *
 from myngspy import *
 import time
 
+SetNumThreads(3)
+
 mesh = Mesh(unit_cube.GenerateMesh(maxh=0.2))
 
 # for i in range(3):
 #     mesh.Refine()
 
 # Draw(mesh)
+order = 5
 
 def doTiming(mesh, cf):
     with TaskManager(pajetrace=10**8):
         t0 = time.time()
-        val = MyIntegrate(cf, mesh, order=2)
-        val2 = Integrate(cf, mesh, order=2)
+        val = MyIntegrate(cf, mesh, order=order)
+        val2 = Integrate(cf, mesh, order=order)
         print(val,val2)
         t1 = time.time()
     return t1-t0
 
 
 print('myCoefficient\t', doTiming(mesh,MyCoefficient()))
-print('x*y          \t', doTiming(mesh,x*y))
-print('x*y (comp)   \t', doTiming(mesh,(x*y).Compile()))
-print('x*y (comp true)\t', doTiming(mesh,(x*y).Compile(True,wait=True)))
+# print('x*y          \t', doTiming(mesh,x*y))
+# print('x*y (comp)   \t', doTiming(mesh,(x*y).Compile()))
+# print('x*y (comp true)\t', doTiming(mesh,(x*y).Compile(True,wait=True)))
